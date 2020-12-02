9337047d06a2b7aac5977456d95c38bc202e72bc
Johnny Taylor <johnny@abledaccess.com>
2020-12-01 19:25:50 -0500

    Update Past Events archive

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,33 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
 
-		sass: {
-			dist: {
-				options: {
-					style: 'compressed'
-				},
-				files: {
-					'styles.css': 'styles.scss'
-				}
-			}
-		},
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'styles.css': 'styles.scss'
+        }
+      }
+    },
 
-		watch: {
-			css: {
-				files: 'styles.scss',
-				tasks: ['sass'],
-			},
-		},
+    watch: {
+      css: {
+        files: 'styles.scss',
+        tasks: ['sass'],
+      },
+    },
 
   });
 
   // Load the plugins.
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default task(s).
   grunt.registerTask('default', ['watch', 'sass']);
